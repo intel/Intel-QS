da113ad8caf13df3d347613f2202a34af07e18ef
xinchuan wu <xinchuanwu@gmail.com>
2019-08-25 12:32:15 -0500

    Change interface_main.

--- interface/qhipster_interface_main.cpp
+++ interface/qhipster_interface_main.cpp
@@ -18,6 +18,7 @@
 #include <unordered_map>
 #include <functional>
 #include <stdexcept>
+#include <cstring>
 
 #include "qureg/qureg.hpp"
 #include "interface_api_qasm.h"
@@ -29,13 +30,24 @@ using namespace std;
 using Type = ComplexDP;
 QubitRegister<Type> *psi1 = nullptr;
 bool fPsiAllocated = false;
-
-
+int num_group = 16;
+int time_limit = 21 * 3600; // seconds
+double remain_time;
+double gate_time = 0;
 
 int main(int argc, char*argv[]) {
+    double t0, t_cur;
+    t0 = sec();
     openqu::mpi::Environment env(argc, argv);
+    MPI_Comm comm = openqu::mpi::Environment::comm();
     string line = "";
     string token = "";
+    double t1, t2;
+    double t_mid;
+    t1 = MPI_Wtime();
+    t2 = 0;
+    int gate_count = 0;
+    char buf[1000];
 
     if (env.is_usefull_rank() == false) 
         return 0;
@@ -44,29 +56,44 @@ int main(int argc, char*argv[]) {
     using Type = ComplexDP;
 
     while(true) {
-        getline(cin,line);
+        if (!myid){
+            getline(cin,line);
+        }
+        std::strcpy(buf,line.c_str());
+        MPI_Bcast(buf, sizeof(buf)/sizeof(char), MPI_CHAR, 0, env.comm());
+        line = buf;
 
+        gate_count++;
+        t_mid = MPI_Wtime();
+        gate_time = t_mid - t2;
+
+        if (psi1){
+            t_cur = sec();
+            if ((t_cur - t0) > (time_limit - gate_time)){
+                psi1->OutputCompressedByteToFile();
+                if (!myid){
+                    cout << "Successfully output compressed blk to file" << endl << flush;
+                }
+                return 0;
+            }
+        }
+        if (!myid){    
+            printf("Total time:\t %1.2f \tgate time:\t %1.2f \tgate count:\t %i\n", t_mid-t1, gate_time, gate_count);
+        }
+
+        t2 = MPI_Wtime();
         if(line.length() >= 1) {
             int token_end = line.find_first_of(' ');
             unsigned long result = 1;
-
             token = line.substr(0,token_end);
-            if(!token.empty()) {
-	/*
-               function<long(string)> func = qufun_table[token];
-               if(func) {
-                  result = func(line.substr(token_end+1,line.length()));
-               }
-*/
-	       result = ExecuteHandler(token,line.substr(token_end+1,line.length()));
-
-               if (result > 0) {
-                   cerr << "Qasm Op failed - ["<<token<<"]"<<endl;
-               }
+            if(!token.empty()) {            
+                result = ExecuteHandler(token,line.substr(token_end+1,line.length()));
+                if (result > 0) {
+                    cerr << "Qasm Op failed - ["<<token<<"]"<<endl;
+                }
             }
         } else
           break;
     }
-
-return 0;
+    return 0;
 }
